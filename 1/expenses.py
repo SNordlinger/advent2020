2df4714cfe28f075852ac829f5b993746cea91c4
Sam Nordlinger <19315410+SNordlinger@users.noreply.github.com>
2020-12-01 00:14:17 -0800

    Fix unused parameter

--- 1/expenses.py
+++ 1/expenses.py
@@ -18,7 +18,7 @@ def find_sum(expenses, target_sum):
 
 def find_triple_sum(expenses, target_sum):
     for i, num in enumerate(expenses[:-1]):
-        remainder = 2020 - num
+        remainder = target_sum - num
         vals = find_sum(expenses[i+1:], remainder)
         if vals is not None:
             vals.append(num)
